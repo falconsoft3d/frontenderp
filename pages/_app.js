0ebfa55be1ee488c622084e4f665309a64e33988
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-10 20:22:00 +0100

    sc

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,11 @@ import 'react-toastify/dist/ReactToastify.css';
 import { AuthContextProvider } from '../src/context/AuthContext';
 import { useRouter } from 'next/router'
 import ProtectedRoute from '../src/components/ProtectedRoute'
-import "bootswatch/dist/materia/bootstrap.min.css"; // Import bootstrap CSS
+// import "bootswatch/dist/materia/bootstrap.min.css"; // Import bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import bootstrap CSS
 import React, { useEffect } from 'react';
+import Script from 'next/script';
+
 
 const noAuthRequired = ['/',
                        '/login',
@@ -15,10 +18,19 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter()
   
   useEffect(() => {
-    require('bootstrap/dist/js/bootstrap.bundle.min.js'); // Import bootstrap JS
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+  
+  useEffect(() => {
+    import("jquery/dist/jquery.min.js");
   }, []);
 
+  useEffect(() => {
+    typeof document !== undefined ? require('bootstrap/dist/js/bootstrap') : null
+  }, [])
+  
   return (
+    <>
     <AuthContextProvider>
       {noAuthRequired.includes(router.pathname) ? (
         <Component {...pageProps} />
@@ -28,6 +40,7 @@ function MyApp({ Component, pageProps }) {
         </ProtectedRoute>
       )}
     </AuthContextProvider>
+    </>
   )
 }
 
